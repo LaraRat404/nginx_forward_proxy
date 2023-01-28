16d366334d70db17993450dcab4bb2dcc2d21458
Lara Rat <115366109+LaraRat404@users.noreply.github.com>
2023-01-28 15:24:30 -0800

    Update environment_setup.sh

--- environment_setup.sh
+++ environment_setup.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-apt install gcc make libssl-dev libpcre3-dev zlib1g-dev libxml2-dev libxslt-dev libgd-dev libgeoip-dev libperl-dev
+apt install gcc make libssl-dev libpcre3-dev zlib1g-dev libxml2-dev libxslt-dev libgd-dev libgeoip-dev libperl-dev -y
 end
