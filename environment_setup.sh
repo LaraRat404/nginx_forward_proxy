7a76767ddec40cb8b4e12ee4a02bcc57d3741875
Lara Rat <115366109+LaraRat404@users.noreply.github.com>
2023-01-28 15:42:26 -0800

    Update environment_setup.sh

--- environment_setup.sh
+++ environment_setup.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
+cd /root
+wget http://nginx.org/download/nginx-1.22.1.tar.gz
+tar xf nginx-1.22.1.tar.gz 
 apt install gcc make libssl-dev libpcre3-dev zlib1g-dev libxml2-dev libxslt-dev libgd-dev libgeoip-dev libperl-dev -y
-end
+cd nginx-1.22.1
+./configure --user=root --group=proxy --sbin=/usr/local/sbin/nginx --with-http_ssl_module --with-http_stub_status_module --with-http_realip_module --with-threads --with-stream --with-stream_ssl_preread_module --with-stream_ssl_module
+make && make install
+nginx -V
