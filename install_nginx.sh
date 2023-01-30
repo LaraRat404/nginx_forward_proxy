15172deca81a72c6016b6f40e042444fdc20eea4
Lara Rat <115366109+LaraRat404@users.noreply.github.com>
2023-01-30 12:34:46 -0800

    Update install_nginx.sh
    2

--- install_nginx.sh
+++ install_nginx.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
+apt install gcc make libssl-dev libpcre3-dev zlib1g-dev -y
+apt install certbot python3-certbot-nginx
 cd /root
 wget http://nginx.org/download/nginx-1.22.1.tar.gz
 tar xf nginx-1.22.1.tar.gz 
-apt install gcc make libssl-dev libpcre3-dev zlib1g-dev -y
 cd nginx-1.22.1
 ./configure --user=root --group=root --sbin-path=/usr/local/sbin/nginx --with-http_ssl_module --with-http_stub_status_module --with-http_realip_module --with-threads --with-stream --with-stream_ssl_preread_module --with-stream_ssl_module
 make && make install
