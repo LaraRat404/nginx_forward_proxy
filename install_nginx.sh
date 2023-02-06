353fb8ad92c926429545d3afcec187cebf6a42f0
Lara Rat <115366109+LaraRat404@users.noreply.github.com>
2023-02-06 10:07:40 -0800

    Update install_nginx.sh

--- install_nginx.sh
+++ install_nginx.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 apt install gcc make libssl-dev libpcre3-dev zlib1g-dev  -y
 apt install certbot python3-certbot-nginx -y
+apt install patch -y
 cd /root
 wget http://nginx.org/download/nginx-1.22.1
 tar xf nginx-1.22.1.tar.gz 
 cd nginx-1.22.1
-./configure --user=root --group=root --sbin-path=/usr/local/sbin/nginx --with-http_ssl_module --with-http_stub_status_module --with-http_realip_module --with-threads --with-stream --with-stream_ssl_preread_module --with-stream_ssl_module 
+git clone https://github.com/chobits/ngx_http_proxy_connect_module.git
+patch -p1 < ./ngx_http_proxy_connect_module/patch/proxy_connect_rewrite_102101.patch
+./configure --user=root --group=root --sbin-path=/sbin/nginx --prefix=/etc/nginx --with-http_ssl_module --with-http_stub_status_module --with-http_realip_module --with-threads --with-stream --with-stream_ssl_preread_module --with-stream_ssl_module --with-http_geoip_module --with-stream_geoip_module --add-module=/root/nginx-1.22.1/ngx_http_proxy_connect_module/patch/proxy_connect.patch 
 make && make install
 nginx -V
