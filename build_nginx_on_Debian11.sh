26392a21b0c82c2d09019b1a97fd3546d967051b
Lara Rat <115366109+LaraRat404@users.noreply.github.com>
2023-02-18 18:04:10 +0100

    Create build_nginx_on_Debian11.sh

---
+++ build_nginx_on_Debian11.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+apt-get install gcc make libssl-dev libpcre3-dev zlib1g-dev -y
+apt-get install patch -y
+cd /root
+wget http://nginx.org/download/nginx-1.22.1.tar.gz
+tar xf nginx-1.22.1.tar.gz 
+cd nginx-1.22.1
+git clone https://github.com/chobits/ngx_http_proxy_connect_module.git
+patch -p1 < ./ngx_http_proxy_connect_module/patch/proxy_connect_rewrite_102101.patch
+./configure --user=root --group=root --sbin-path=/sbin/nginx --prefix=/etc/nginx --with-http_ssl_module --with-http_stub_status_module --with-http_realip_module --with-threads --with-stream --with-stream_ssl_preread_module --with-stream_ssl_module --add-module=/root/nginx-1.22.1/ngx_http_proxy_connect_module 
+make
+make install
+# Next string save nginx.conf backup.
+cp /etc/nginx/conf/nginx.conf /etc/nginx/conf/nginx.conf.bak
+# You can use www o www-data user:group, worker_processes = processosr cores.
+cat <<\EOF > /etc/nginx/conf/nginx.conf
+user  root root;
+worker_processes  2;
+events {
+    worker_connections  1024;
+}
+http {
+        include       mime.types;
+        default_type  application/octet-stream;
+        sendfile        on;
+        keepalive_timeout  65;
+        server {
+                listen 8888;
+                resolver 8.8.8.8;
+                proxy_connect;
+                proxy_connect_allow     443;
+                proxy_connect_connect_timeout 20s;
+                proxy_connect_read_timeout 20s;
+                proxy_connect_send_timeout 20s;
+                }
+}
+EOF
+nginx
